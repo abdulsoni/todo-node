13bbd41bbeb67ecc8a2157eb6fd25b9bb97e0ce7
abdul <abc@gmail.com>
2019-10-24 12:50:15 +0530

    todo test

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const todoRoutes = require("./routes/todos");
 
 const app = express();
 
-// mongoose.connect("mongodb://"+process.env.MONGO_ATLAS_DB_user+":" +
-// process.env.MONGO_ATLAS_DB_password + 
-// "noBound6@ds137508.mlab.com:37508/todo-test", { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     console.log(chalk.blue.bold('-::Database connected::-'));
-//   });
-
-  mongoose.connect('mongodb://localhost/test',{ useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log(chalk.blue.bold('-::Database connected::-'));
-    });
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function callback () {
-    console.log("h");
+mongoose.connect("mongodb://"+process.env.MONGO_ATLAS_DB_user+":" +
+process.env.MONGO_ATLAS_DB_password + 
+"noBound6@ds137508.mlab.com:37508/todo-test", { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log(chalk.blue.bold('-::Database connected::-'));
   });
+
+  // mongoose.connect('mongodb://localhost/test',{ useNewUrlParser: true, useUnifiedTopology: true },
+  //   () => {
+  //     console.log(chalk.blue.bold('-::Database connected::-'));
+  //   });
+  // var db = mongoose.connection;
+  // db.on('error', console.error.bind(console, 'connection error:'));
+  // db.once('open', function callback () {
+  //   console.log("h");
+  // });
   mongoose.Promise = global.Promise;
 
 app.use('/public', express.static('public'));
