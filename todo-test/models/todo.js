1fd6e2d9697407beeae25247873eaf013d2181dd
abdul <abc@gmail.com>
2019-10-24 12:34:02 +0530

    first Todo Test

---
+++ todo-test/models/todo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    isDone: {
+      type: Boolean,
+      default: false
+    }
+  });
+
+module.exports = mongoose.model('Todo', TodoSchema);
